e4b4b4d7826c66fa800ec8d3a1ac271bb3484da1
Renata Rodrigues <ticare1@hotmail.com>
2026-05-17 21:02:01 -0300

    Configuracao do LocalDB em appsettings.json e Registro do DbContext em program.cs

--- ControleEstoque/Program.cs
+++ ControleEstoque/Program.cs
@@ -1,3 +1,8 @@
+using ControleEstoque.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,12 +10,26 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Configuração da cultura da aplicação
+var supportedCultures = new[] { new CultureInfo("pt-BR") };
+builder.Services.Configure<RequestLocalizationOptions>(options => {
+    options.DefaultRequestCulture = new RequestCulture("pt-BR");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+});
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseRequestLocalization(); // Ativa a localização para tratar a vírgula corretamente
 
 app.UseHttpsRedirection();
 app.UseRouting();
